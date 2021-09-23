4ea873191e141fba9e9c71ffa86c51001d7d5b64
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-09-22 21:45:10 -0300

    finish aulaApi project

--- aulaApi/App.js
+++ aulaApi/App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Image, Text, View, StyleSheet } from 'react-native';
 
-export default function App() {
+export default () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getDogs = async () => {
+     try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const json = await response.json();
+      // console.log(JSON.stringify(json));
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getDogs();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Cachorros aleatórios</Text>
+      {isLoading ? <ActivityIndicator/> : (
+        <Image source={
+          {uri:data.message}}
+          style={styles.image}
+          />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
   },
-});
+  image: {
+    width: 300,
+    height: 300
+  },
+  text: {
+    fontSize: 25,
+    marginBottom: 30,
+    fontFamily: 'sans-serif-condensed',
+    textDecorationLine: "underline"
+  }
+}) 
